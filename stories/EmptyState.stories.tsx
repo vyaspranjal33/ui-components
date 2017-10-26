3183e381f95f8d238b73a5dec0f9de4cbf67e58a
Chris Chalstrom <chris.chalstrom@gmail.com>
2017-10-26 15:25:35 -0600

    mk ci-lint cmd (#34)

--- stories/EmptyState.stories.tsx
+++ stories/EmptyState.stories.tsx
@@ -1,16 +1,16 @@
-import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
-import EmptyState from '../src/components/EmptyState';
 import Button from '../src/components/Button';
 import ButtonList from '../src/components/ButtonList';
+import EmptyState from '../src/components/EmptyState';
 import Footer from '../src/components/SmallFooter';
 
 const stories = storiesOf('Empty State', module);
 
 stories.add('Empty State With Icon', () => (
-  <EmptyState icon='contacts' header='Get started adding contacts'>
+  <EmptyState icon="contacts" header="Get started adding contacts">
     <p>
       Add and update your contacts via CSV upload or manually add them one at a time,
       then create static lists or dynamic segments for better targeting.  Use the
@@ -20,7 +20,7 @@ stories.add('Empty State With Icon', () => (
 ));
 
 stories.add('Empty State With Buttons', () => (
-  <EmptyState header='Get started adding contacts'>
+  <EmptyState header="Get started adding contacts">
     <p>
       Add and update your contacts via CSV upload or manually add them one at a time,
       then create static lists or dynamic segments for better targeting.  Use the
@@ -39,7 +39,7 @@ stories.add('Empty State With Buttons', () => (
 
 stories.add('Empty State With Everything', () => (
   <div>
-    <EmptyState icon='contacts' header='Get started adding contacts'>
+    <EmptyState icon="contacts" header="Get started adding contacts">
       <p>
         Add and update your contacts via CSV upload or manually add them one at a time,
         then create static lists or dynamic segments for better targeting.  Use the
@@ -54,10 +54,10 @@ stories.add('Empty State With Everything', () => (
         </Button>
       </ButtonList>
     </EmptyState>
-    <Footer icon='help' header='Need help?'>
-      <a href='#'>
+    <Footer icon="help" header="Need help?">
+      <a href="#">
         Get more info
-      </a>{ ' ' }
+      </a>{' '}
       on adding and updating contacts.
     </Footer>
   </div>
--- stories/SmallFooter.stories.tsx
+++ stories/SmallFooter.stories.tsx
@@ -6,22 +6,22 @@ import Footer from '../src/components/SmallFooter';
 const stories = storiesOf('Small Footer', module);
 
 stories.add('Footer Simple', () => (
-  <Footer icon='help' header='What the what?'>
+  <Footer icon="help" header="What the what?">
     This component is just fantastic.
   </Footer>
 ));
 
 stories.add('Need Help Footer Simple', () => (
-  <Footer icon='help' header='Need help?'>
+  <Footer icon="help" header="Need help?">
     Get more info on adding and updating contacts.
   </Footer>
 ));
 
 stories.add('Need Help Footer with Link', () => (
-  <Footer icon='help' header='Need help?'>
-    <a href='#'>
+  <Footer icon="help" header="Need help?">
+    <a href="#">
       Get more info
-    </a>{ ' ' }
+    </a>{' '}
     on adding and updating contacts.
   </Footer>
 ));
