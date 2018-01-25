3305f4bd08d061591af0875b20f5f00ac6b5c4d4
Eric Tjossem <erictj@gmail.com>
2018-01-25 11:24:02 -0800

    [FEE-17] Adds a Table component (#77)
    * Add Table component
    * Allows use of Table, Cell, Row, TableBody, TableHeader, and HeaderCell

---
+++ src/components/TableHeaderCell.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cn from '../utilities/classnames';
+
+export interface TableCellProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export class HeaderCell extends React.Component<TableCellProps> {
+  public render() {
+    const classes = this.props.className ? this.props.className : ""
+    
+    return (
+      <th className={classes}>
+        {this.props.children}
+      </th>
+    );
+  }
+};
+
+export default HeaderCell;
