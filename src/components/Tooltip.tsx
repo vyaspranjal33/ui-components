8a54a9b3be58953ae78cac9f3a18b0315cb9dab5
Alex Thomsen <alex.scott.thomsen@gmail.com>
2018-01-29 14:08:09 -0800

    Tooltip Refactors, Story refactor, Updated HTML Tooltip to be stateful

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,43 +1,105 @@
 import React from 'react';
 import cn from '../utilities/classnames';
 
+export type TooltipDirection = 'up' | 'down' | 'left' | 'right';
+
 export interface TooltipProps {
   content?: string;
-  direction?: string;
+  direction?: TooltipDirection;
   className?: string;
+  children?: React.ReactElement<any>;
 }
 
-export interface HTMLTooltipProps {
-  parent?: string;
-  isLeft?: boolean;
-  className?: string;
-}
-
-export class Tooltip extends React.Component<TooltipProps> {
-  public render() {
-    const classes = this.props.className ? this.props.className : ""
-    const content = this.props.content
-    const direction = this.props.direction
-    const children = this.props.children
-
-    return (
-      <span className={classes} data-tooltip={content} data-tooltip-pos={direction}>{children}</span>
-    );
-
-  }
+export const Tooltip: React.SFC<TooltipProps> = ({
+  content,
+  direction,
+  className,
+  children,
+}) => {
+  return (
+    <span
+      className={className}
+      data-tooltip={content}
+      data-tooltip-pos={direction}
+    >
+      {children}
+    </span>
+  );
 };
 
-export class HTMLTooltip extends React.Component<HTMLTooltipProps> {
-  public render() {
-    const parent = {"__html": this.props.parent}
-    const children = this.props.children
-    const isLeft = this.props.isLeft
-    const classes = this.props.className ? this.props.className + " tooltip-js-content" : "tooltip-js-content"
+export type HtmlTooltipDirection = 'left' | 'right';
 
+export interface HTMLTooltipProps {
+  direction?: TooltipDirection;
+  className?: string;
+  children?: React.ReactElement<any>;
+  hoverTarget?: React.ReactElement<any>;
+}
+
+export interface HTMLTooltipState {
+  hovered: boolean;
+  tooltipHeight: number;
+}
+
+export class HTMLTooltip extends React.Component<
+  HTMLTooltipProps,
+  HTMLTooltipState
+> {
+  private tooltipRef: HTMLDivElement;
+
+  constructor(props: HTMLTooltipProps) {
+    super(props);
+
+    this.state = {
+      hovered: false,
+      tooltipHeight: 0,
+    };
+
+    this.handleHoverIn = this.handleHoverIn.bind(this);
+    this.handleHoverOut = this.handleHoverOut.bind(this);
+  }
+
+  public static defaultProps = {
+    hovered: false,
+    direction: 'right',
+    className: '',
+  };
+
+  public state = {
+    hovered: false,
+    tooltipHeight: 0,
+  };
+
+  public handleHoverIn = () => {
+    this.setState({ hovered: true }, () => {
+      this.setState({ tooltipHeight: this.tooltipRef.offsetHeight });
+    });
+  };
+
+  public handleHoverOut = () => {
+    setTimeout(() => {
+      this.setState({ hovered: false });
+    }, 1000);
+  };
+
+  render() {
     return (
-      <div>
-        <div className="tooltip-js-parent" dangerouslySetInnerHTML={parent}></div>
-        <div className={classes}>{children}</div>
+      <div onMouseOut={this.handleHoverOut}>
+        <div className="tooltip-js-parent" onMouseOver={this.handleHoverIn}>
+          {this.props.hoverTarget}
+        </div>
+        <div
+          className={cn(`tooltip-js-content ${this.props.className}`, {
+            'is-left': this.props.direction === 'left',
+            'is-visible': this.state.hovered,
+          })}
+          style={{ top: -(this.state.tooltipHeight / 2) - 3 }}
+          ref={input => {
+            this.tooltipRef = input;
+          }}
+        >
+          {this.props.children}
+        </div>
       </div>
     );
   }
