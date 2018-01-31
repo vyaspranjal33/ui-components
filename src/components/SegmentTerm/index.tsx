443b499d96f8e747292f9b5f5eacf51d07a1a2fb
Chris Chalstrom <chris.chalstrom@gmail.com>
2018-01-31 16:50:49 -0700

    🥙 Segment Term updates #patch (#87) 🍫
    * pair w jason to update segment terms
    
    * use more render proppy way for seg terms
    
    * cr comments

--- src/components/SegmentTerm/index.tsx
+++ src/components/SegmentTerm/index.tsx
@@ -1,41 +1,102 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
 import cn from '../../utilities/classnames';
+import { Button } from '../Button';
+import { ButtonList } from '../ButtonList';
 import { Icon } from '../Icon';
-export { SegmentTermControls } from './SegmentTermControls';
-export { SegmentTermDescription } from './SegmentTermDescription';
-export { SegmentTermTitle } from './SegmentTermTitle';
-
-export interface SegmentTermWrapProps {
-  children: React.ReactNode;
-}
-
-export const SegmentTermWrap: React.SFC<SegmentTermWrapProps> = ({ children }) => (
-  <section className="segment-term-wrap">
-    {children}
-  </section>
-);
 
 export interface SegmentTermProps {
-  children: React.ReactNode;
+  hasAddButton?: boolean;
   hasSeparator?: boolean;
+  isEditable?: boolean;
   isEditing?: boolean;
-  onCreate?: (e: any) => void;
+  label?: string;
+  onAddButtonClick?: (e: any) => void;
+  onCancel?: (e: any) => void;
+  onConfirm?: (e: any) => void;
+  onEdit?: (e: any) => void;
+  queryName?: string;
+  renderInputs?: () => React.ReactNode;
+  showConfirm?: boolean;
+  title: string;
 }
 
-export const SegmentTerm: React.SFC<SegmentTermProps> = ({ children, hasSeparator, isEditing, onCreate }) => (
-  <span className={cn('segment-term', { 'has-separator': hasSeparator })}>
-    <section className={cn('segment-term-inputs', { 'is-editable': isEditing })}>
-      {children}
-    </section>
-    {
-      !hasSeparator &&
-        <Icon type="create" onClick={isEditing ? undefined : onCreate} />
-    }
-  </span>
-);
+export class SegmentTerm extends PureComponent<SegmentTermProps> {
+  public static defaultProps = {
+    hasAddButton: false,
+    hasSeparator: false,
+    isEditable: false,
+    isEditing: false,
+  };
 
-SegmentTerm.defaultProps = {
-  hasSeparator: false,
-  isEditing: false,
-};
+  public get termControls(): React.ReactNode {
+    const { isEditable, isEditing, onCancel, onConfirm, showConfirm } = this.props;
+
+    if (isEditing) {
+      return (
+        <ButtonList>
+          {
+            showConfirm &&
+              <Button type="secondary" small onClick={onConfirm}>
+                <Icon type="check-thin" />
+              </Button>
+          }
+          <Button type="danger" small onClick={onCancel}>
+            <Icon type="x" />
+          </Button>
+        </ButtonList>
+      );
+    }
+
+    if (isEditable) {
+      return <Icon type="pencil" className="segment-term-edit" />;
+    }
+
+    return null;
+  }
+
+  public render() {
+    const {
+      hasAddButton,
+      hasSeparator,
+      isEditable,
+      isEditing,
+      label,
+      onAddButtonClick,
+      onEdit,
+      queryName,
+      renderInputs,
+      title,
+    } = this.props;
+
+    return (
+      <div className="segment-term-wrap">
+        <p className="segment-term-title">
+          {title}
+        </p>
+        <div
+          className={cn('segment-term', { 'has-separator': hasSeparator, 'is-editable': isEditing })}
+          onClick={(isEditable && !isEditing) ? onEdit : undefined}
+        >
+        {isEditing && renderInputs && renderInputs()}
+        {
+          !isEditing &&
+            <p>
+              {`${label} `}
+              <strong>{queryName}</strong>
+            </p>
+        }
+        {this.termControls}
+        </div>
+        {
+          hasAddButton &&
+            <ButtonList>
+              <Button type="secondary" icon="plus" onClick={onAddButtonClick}>
+                Add Condition
+              </Button>
+            </ButtonList>
+        }
+      </div>
+    );
+  }
+}
