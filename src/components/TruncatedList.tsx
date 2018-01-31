8a16e32b767a539bad46803cae96076378e73353
Eric Tjossem <eric.tjossem@sendgrid.com>
2018-01-31 12:55:37 -0800

    Add TruncatedList

--- src/components/TruncatedList.tsx
+++ src/components/TruncatedList.tsx
@@ -2,24 +2,26 @@ import React from 'react';
 import cn from '../utilities/classnames';
 
 export interface TruncatedListProps {
-  list?: Array<string>;
+  list?: string[];
   limit?: number;
   className?: string;
+  link?: string;
 }
 
 export const TruncatedList: React.SFC<TruncatedListProps> = ({
   list,
   limit = 3,
   className,
+  link,
 }) => {
   const shown = list.slice(0, limit);
   const truncated = list.slice(limit);
-  const shownContent = shown.join(", ");
+  const shownContent = shown.join(', ');
   const truncatedLength = truncated.length.toString();
 
   return (
     <span className={cn(className)}>
-      {shownContent}{(truncated.length > 0) ? <span>, <abbr><a href='#'>+{truncatedLength} more</a></abbr></span> : ''}
+      {shownContent}{(truncated.length > 0) ? <span>, <abbr><a href={link}>+{truncatedLength} more</a></abbr></span> : ''}
     </span>
   );
 };
--- stories/TruncatedList.stories.tsx
+++ stories/TruncatedList.stories.tsx
@@ -5,25 +5,28 @@ import React from 'react';
 import TruncatedList from '../src/components/TruncatedList';
 
 const stories = storiesOf('TruncatedList', module);
-const singleItemList = ["Boromir"]
-const list = ["Boromir", "Elrond", "Frodo", "Gimli", "Legolas"]
-const limit = 2 // Default is 3
+const singleItemList = ['Boromir'];
+const list = ['Boromir', 'Elrond', 'Frodo', 'Gimli', 'Legolas'];
+const limit = 2; // Default is 3
 
 stories.add('5 strings, limit 2', () => (
   <TruncatedList
     list={list}
     limit={limit}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
 
 stories.add('5 strings, limit default', () => (
   <TruncatedList
     list={list}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
 
 stories.add('1 string, limit default', () => (
   <TruncatedList
     list={singleItemList}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
