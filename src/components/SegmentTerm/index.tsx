fc7fa061889f6c8d463c4aaca2237d77c440b2fc
Chris Chalstrom <chris.chalstrom@gmail.com>
2018-01-31 08:40:59 -0700

    use more render proppy way for seg terms

--- src/components/SegmentTerm/index.tsx
+++ src/components/SegmentTerm/index.tsx
@@ -4,45 +4,86 @@ import cn from '../../utilities/classnames';
 import { Button } from '../Button';
 import { ButtonList } from '../ButtonList';
 import { Icon } from '../Icon';
-export { SegmentTermControls } from './SegmentTermControls';
-export { SegmentTermDescription } from './SegmentTermDescription';
-export { SegmentTermTitle } from './SegmentTermTitle';
-
-export interface SegmentTermWrapProps {
-  children: React.ReactNode;
-}
-
-export const SegmentTermWrap: React.SFC<SegmentTermWrapProps> = ({ children }) => (
-  <div className="segment-term-wrap">
-    {children}
-  </div>
-);
 
 export interface SegmentTermProps {
-  children: React.ReactNode;
+  hasAddButton?: boolean;
   hasSeparator?: boolean;
+  isEditable?: boolean;
   isEditing?: boolean;
+  label?: string;
+  onAddButtonClick?: (e: any) => void;
+  onCancel?: (e: any) => void;
+  onConfirm?: (e: any) => void;
+  onEdit?: (e: any) => void;
+  queryName?: string;
+  renderInputs?: () => React.ReactNode;
+  showConfirm?: boolean;
+  title: string;
 }
 
-export interface SegmentTermAddButtonProps {
-  onClick: () => void;
-}
-
-export const SegmentTermAddButton: React.SFC<SegmentTermAddButtonProps> = ({ onClick }) => (
-  <ButtonList>
-    <Button type="secondary" icon="plus" onClick={onClick}>
-      Add Condition
-    </Button>
-  </ButtonList>
-);
-
-export const SegmentTerm: React.SFC<SegmentTermProps> = ({ children, hasSeparator, isEditing }) => (
-  <div className={cn('segment-term', { 'has-separator': hasSeparator, 'is-editable': isEditing })}>
-    {children}
+export const SegmentTerm: React.SFC<SegmentTermProps> = ({
+  hasAddButton,
+  hasSeparator,
+  isEditable,
+  isEditing,
+  label,
+  onAddButtonClick,
+  onCancel,
+  onConfirm,
+  onEdit,
+  queryName,
+  renderInputs,
+  showConfirm,
+  title,
+}) => (
+  <div className="segment-term-wrap">
+    <p className="segment-term-title">
+      {title}
+    </p>
+    <div
+      className={cn('segment-term', { 'has-separator': hasSeparator, 'is-editable': isEditing })}
+      onClick={(isEditable && !isEditing) ? onEdit : undefined}
+    >
+    {isEditing && renderInputs && renderInputs()}
+    {
+      !isEditing &&
+        <p>
+          {`${label} `}
+          <strong>{queryName}</strong>
+        </p>
+    }
+    {
+      isEditing ? (
+        <ButtonList>
+          {
+            showConfirm &&
+              <Button type="secondary" small onClick={onConfirm}>
+                <Icon type="check-thin" />
+              </Button>
+          }
+          <Button type="danger" small onClick={onCancel}>
+            <Icon type="x" />
+          </Button>
+        </ButtonList>
+      ) : (
+        isEditable && <Icon type="pencil" className="segment-term-edit" />
+      )
+    }
+    </div>
+    {
+      hasAddButton &&
+        <ButtonList>
+          <Button type="secondary" icon="plus" onClick={onAddButtonClick}>
+            Add Condition
+          </Button>
+        </ButtonList>
+    }
   </div>
 );
 
 SegmentTerm.defaultProps = {
+  hasAddButton: false,
   hasSeparator: false,
+  isEditable: false,
   isEditing: false,
 };
--- stories/SegmentTerms.stories.tsx
+++ stories/SegmentTerms.stories.tsx
@@ -2,109 +2,105 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React, { Fragment } from 'react';
 
-import {
-  SegmentTerm,
-  SegmentTermAddButton,
-  SegmentTermControls,
-  SegmentTermDescription,
-  SegmentTermTitle,
-  SegmentTermWrap,
-} from '../src/components/SegmentTerm';
+import { SegmentTerm } from '../src/components/SegmentTerm';
 
 const stories = storiesOf('Segment Term', module);
-const onClick = () => {};
+const onAddButtonClick = action('add button click');
+const onCancelClick = action('cancel click');
+const onConfirmClick = action('confirm click');
+const onEditClick = action('on edit click');
 
 stories.add('Segment terms', () => (
-  <Fragment>
-    <SegmentTermWrap>
-      <SegmentTermTitle>
-        Entry Criteria
-      </SegmentTermTitle>
-      <SegmentTerm hasSeparator>
-        <SegmentTermDescription
-          label="The first time a contact is added to"
-          queryName="People in Los Angeles"
-        />
-        <SegmentTermControls isEditable />
-      </SegmentTerm>
-    </SegmentTermWrap>
-    <SegmentTermWrap>
-      <SegmentTermTitle>
-        Or
-      </SegmentTermTitle>
-      <SegmentTerm>
-        <SegmentTermDescription
-          label="Every time a contact is added to"
-          queryName="People in Denver"
-        />
-        <SegmentTermControls isEditable />
-      </SegmentTerm>
-      <SegmentTermAddButton onClick={onClick} />
-    </SegmentTermWrap>
-  </Fragment>
+  <div>
+    <SegmentTerm
+      title="Entry Criteria"
+      hasSeparator
+      label="The first time a contact is added to"
+      queryName="People in Los Angeles"
+      isEditable
+      onEdit={onEditClick}
+    />
+    <SegmentTerm
+      title="Or"
+      label="Every time a contact is added to"
+      queryName="People in Denver"
+      isEditable
+      hasAddButton
+      onAddButtonClick={onAddButtonClick}
+      onEdit={onEditClick}
+    />
+  </div>
 ));
 
-stories.add('Segment terms which cannot be edited', () => (
-  <SegmentTermWrap>
-    <SegmentTermTitle>
-      Entry Criteria
-    </SegmentTermTitle>
-    <SegmentTerm>
-      <SegmentTermDescription
-        label="The first time a contact is added to"
-        queryName="People in Los Angeles"
-      />
-    </SegmentTerm>
-  </SegmentTermWrap>
+stories.add('Segment term which cannot be edited', () => (
+  <SegmentTerm
+    title="Entry Criteria"
+    label="The first time a contact is added to"
+    queryName="People in Los Angeles"
+    isEditable={false}
+  />
 ));
 
-stories.add('Segment terms which is being edited', () => (
-  <SegmentTermWrap>
-    <SegmentTermTitle>
-      Entry Criteria
-    </SegmentTermTitle>
-    <SegmentTerm hasSeparator isEditing>
-      <div className="input-select-wrap">
-        <label className="input-select-label" htmlFor="select-a">Select send conditions</label>
-        <select id="select-a">
-          <option>The first time a contact</option>
-        </select>
-      </div>
-      <div className="input-select-wrap">
-        <label className="input-select-label" htmlFor="select-b">Select contact criteria</label>
-        <select id="select-b">
-          <option>Abandons a cart</option>
-        </select>
-      </div>
-      <SegmentTermControls isEditing />
-    </SegmentTerm>
-    <SegmentTermAddButton onClick={onClick} />
-  </SegmentTermWrap>
+stories.add('Segment term which is being edited', () => (
+  <SegmentTerm
+    title="Entry Criteria"
+    hasAddButton
+    hasSeparator
+    isEditable
+    isEditing
+    onAddButtonClick={onAddButtonClick}
+    onCancel={onCancelClick}
+    onConfirm={onConfirmClick}
+    renderInputs={() => (
+      <Fragment>
+        <div className="input-select-wrap">
+          <label className="input-select-label" htmlFor="select-a">Select send conditions</label>
+          <select id="select-a">
+            <option>The first time a contact</option>
+          </select>
+        </div>
+        <div className="input-select-wrap">
+          <label className="input-select-label" htmlFor="select-b">Select contact criteria</label>
+          <select id="select-b">
+            <option>Abandons a cart</option>
+          </select>
+        </div>
+      </Fragment>
+    )}
+  />
 ));
 
-stories.add('Segment terms which is being edited and is filled out', () => (
-  <SegmentTermWrap>
-    <SegmentTermTitle>
-      Entry Criteria
-    </SegmentTermTitle>
-    <SegmentTerm hasSeparator isEditing>
-      <div className="input-select-wrap">
-        <select id="select-a">
-          <option>The first time a contact</option>
-        </select>
-      </div>
-      <div className="input-select-wrap">
-        <select id="select-b">
-          <option>Abandons a cart</option>
-        </select>
-      </div>
-      <div className="input-select-wrap">
-        <select id="select-c">
-          <option>People in Los Angeles</option>
-        </select>
-      </div>
-      <SegmentTermControls isEditing showConfirm />
-    </SegmentTerm>
-    <SegmentTermAddButton onClick={onClick} />
-  </SegmentTermWrap>
+stories.add('Segment term which is being edited and is filled out', () => (
+  <SegmentTerm
+    title="Entry Criteria"
+    hasAddButton
+    hasSeparator
+    isEditable
+    isEditing
+    showConfirm
+    onAddButtonClick={onAddButtonClick}
+    onCancel={onCancelClick}
+    onConfirm={onConfirmClick}
+    renderInputs={() => (
+      <Fragment>
+        <div className="input-select-wrap">
+          <label className="input-select-label" htmlFor="select-a">Select send conditions</label>
+          <select id="select-a">
+            <option>The first time a contact</option>
+          </select>
+        </div>
+        <div className="input-select-wrap">
+          <label className="input-select-label" htmlFor="select-b">Select contact criteria</label>
+          <select id="select-b">
+            <option>Abandons a cart</option>
+          </select>
+        </div>
+        <div className="input-select-wrap">
+          <select id="select-c">
+            <option>People in Los Angeles</option>
+          </select>
+        </div>
+      </Fragment>
+    )}
+  />
 ));
