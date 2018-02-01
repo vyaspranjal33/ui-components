47c91b610b7a796e18cbf1ad6cd80f43aa2c3d51
Eric Tjossem <eric.tjossem@sendgrid.com>
2018-02-01 13:56:27 -0800

    Update tests

--- stories/Grid.stories.tsx
+++ stories/Grid.stories.tsx
@@ -6,7 +6,6 @@ import { Card } from '../src/components/Card';
 import Divider from '../src/components/Divider';
 import { Column } from '../src/components/Grid/Column';
 import { Row } from '../src/components/Grid/Row';
-import Divider from '../src/components/Divider';
 
 const stories = storiesOf('Grid', module);
 
