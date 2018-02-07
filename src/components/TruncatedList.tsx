6c41234d9ee589a49e782146f874473f253b2850
Eric Tjossem <eric.tjossem@sendgrid.com>
2018-02-07 15:30:47 -0800

    Split out TruncatedHTMLList

--- src/components/TruncatedList.tsx
+++ src/components/TruncatedList.tsx
@@ -1,30 +1,59 @@
 import React from 'react';
 import cn from '../utilities/classnames';
 
-export interface TruncatedListProps {
-  list?: string[];
+export interface TruncatedInlineListProps {
+  items?: string[];
   limit?: number;
   className?: string;
   link?: string;
 }
 
-export const TruncatedList: React.SFC<TruncatedListProps> = ({
-  list,
+export interface TruncatedHTMLListProps {
+  items?: object[];
+  limit?: number;
+  className?: string;
+  link?: string;
+  itemRenderer?: (item: object) => any;
+  moreRenderer?: (truncatedCount: string, link: string) => any;
+}
+
+export const TruncatedInlineList: React.SFC<TruncatedInlineListProps> = ({
+  items,
   limit = 3,
   className,
   link,
 }) => {
-  const shown = list.slice(0, limit);
-  const truncated = list.slice(limit);
-  const shownContent = shown.join(', ');
+  const shown = items.slice(0, limit);
+  const truncated = items.slice(limit);
   const truncatedLength = truncated.length.toString();
-
+  const shownText = shown.join(', ');
   return (
     <span className={cn(className)}>
-      {shownContent}
+      {shownText}
       {(truncated.length > 0) ? <span>, <abbr><a href={link}>+{truncatedLength} more</a></abbr></span> : ''}
     </span>
   );
 };
 
-export default TruncatedList;
+export const TruncatedHTMLList: React.SFC<TruncatedHTMLListProps> = ({
+  items,
+  limit = 3,
+  className,
+  link,
+  itemRenderer,
+  moreRenderer,
+}) => {
+  const shown = items.slice(0, limit);
+  const truncated = items.slice(limit);
+  const truncatedLength = truncated.length.toString();
+  const shownHTML = shown.map((item) => itemRenderer(item));
+  const moreHTML = moreRenderer(truncatedLength, link);
+  return(
+    <div>
+      {shownHTML}
+      {moreHTML}
+    </div>
+  );
+};
+
+export default TruncatedInlineList;
--- stories/TruncatedList.stories.tsx
+++ stories/TruncatedList.stories.tsx
@@ -2,31 +2,57 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
-import TruncatedList from '../src/components/TruncatedList';
+import { TruncatedInlineList, TruncatedHTMLList } from '../src/components/TruncatedList';
 
-const stories = storiesOf('TruncatedList', module);
+const stories = storiesOf('Truncated List', module);
 const singleItemList = ['Boromir'];
 const list = ['Boromir', 'Elrond', 'Frodo', 'Gimli', 'Legolas'];
 const limit = 2; // Default is 3
+const objectList = [
+  {name: "Boromir", dies: "Yep."},
+  {name: "Elrond", dies: "Nah!"},
+  {name: "Frodo", dies: "Nah!"},
+  {name: "Gimli", dies: "Nah!"},
+  {name: "Legolas", dies: "Nah!"}
+];
 
-stories.add('5 strings, limit 2', () => (
-  <TruncatedList
-    list={list}
+stories.add('Inline - Limit 2', () => (
+  <TruncatedInlineList
+    items={list}
     limit={limit}
     link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
 
-stories.add('5 strings, limit default', () => (
-  <TruncatedList
-    list={list}
+stories.add('Inline - Limit Default', () => (
+  <TruncatedInlineList
+    items={list}
     link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
 
-stories.add('1 string, limit default', () => (
-  <TruncatedList
-    list={singleItemList}
+stories.add('Inline - Only 1 Item', () => (
+  <TruncatedInlineList
+    items={singleItemList}
     link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
+
+stories.add('HTML - Custom Renderers', () => (
+  <TruncatedHTMLList
+    items={objectList}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
+    itemRenderer={(item: object) => (
+      <div>
+        <h1>{item["name"]}</h1>
+        <p>Dies in the movies? {item["dies"]}</p>
+      </div>
+    )}
+    moreRenderer={(truncatedCount: string, link: string) => (
+      <div>
+        <a href={link}>Plus {truncatedCount} more!</a>
+      </div>
+    )}
+  />
+));
+
