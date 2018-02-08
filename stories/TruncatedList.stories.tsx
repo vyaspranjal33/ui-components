e7e0b2529e31e1bb8b12968fd20f97fbac77c7c8
Steve Kinney <hello@stevekinney.net>
2018-02-08 10:17:12 -0700

    Suggestions for TruncatedList (#97)
    * Refactor TruncatedList

--- stories/TruncatedList.stories.tsx
+++ stories/TruncatedList.stories.tsx
@@ -2,57 +2,59 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 
-import { TruncatedInlineList, TruncatedHTMLList } from '../src/components/TruncatedList';
+import { TruncatedList } from '../src/components/TruncatedList';
 
 const stories = storiesOf('Truncated List', module);
 const singleItemList = ['Boromir'];
 const list = ['Boromir', 'Elrond', 'Frodo', 'Gimli', 'Legolas'];
 const limit = 2; // Default is 3
 const objectList = [
-  {name: "Boromir", dies: "Yep."},
-  {name: "Elrond", dies: "Nah!"},
-  {name: "Frodo", dies: "Nah!"},
-  {name: "Gimli", dies: "Nah!"},
-  {name: "Legolas", dies: "Nah!"}
+  { name: 'Boromir', dies: true },
+  { name: 'Elrond', dies: false },
+  { name: 'Frodo', dies: false },
+  { name: 'Gimli', dies: false },
+  { name: 'Legolas', dies: false },
 ];
 
-stories.add('Inline - Limit 2', () => (
-  <TruncatedInlineList
+stories.add('Standard - Limit 2', () => (
+  <TruncatedList
     items={list}
     limit={limit}
     link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
 
-stories.add('Inline - Limit Default', () => (
-  <TruncatedInlineList
+stories.add('Standard - Limit Default', () => (
+  <TruncatedList
     items={list}
     link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
 
-stories.add('Inline - Only 1 Item', () => (
-  <TruncatedInlineList
+stories.add('Standard - Only 1 Item', () => (
+  <TruncatedList
     items={singleItemList}
     link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
   />
 ));
 
-stories.add('HTML - Custom Renderers', () => (
-  <TruncatedHTMLList
+stories.add('Custom Render Props', () => (
+  <TruncatedList
     items={objectList}
     link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
-    itemRenderer={(item: object) => (
-      <div>
-        <h1>{item["name"]}</h1>
-        <p>Dies in the movies? {item["dies"]}</p>
-      </div>
+    renderItems={items => (
+      <>
+        {items.map(({ name, dies }) => (
+          <span key={name}>
+            {name} { dies && '(Dead)' },{' '}
+          </span>
+        ))}
+      </>
     )}
-    moreRenderer={(truncatedCount: string, link: string) => (
-      <div>
-        <a href={link}>Plus {truncatedCount} more!</a>
-      </div>
+    renderMore={(numberOfItems: string, link: string) => (
+      <>
+        <a href={link}>and {numberOfItems} more…</a>
+      </>
     )}
   />
 ));
-
