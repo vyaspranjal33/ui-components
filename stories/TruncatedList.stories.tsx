f03e80192bec91b5b1e2a25a2b98a158c0c12ac4
Eric Tjossem <erictj@gmail.com>
2018-02-08 12:57:45 -0800

    Component: Truncated Lists #patch (#88)
    * Add TruncatedList
    
    * Add TruncatedList
    
    * Minor tweak
    
    * Split out TruncatedHTMLList
    
    * Suggestions for TruncatedList (#97)
    
    * Refactor TruncatedList

---
+++ stories/TruncatedList.stories.tsx
@@ -0,0 +1,60 @@
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import { TruncatedList } from '../src/components/TruncatedList';
+
+const stories = storiesOf('Truncated List', module);
+const singleItemList = ['Boromir'];
+const list = ['Boromir', 'Elrond', 'Frodo', 'Gimli', 'Legolas'];
+const limit = 2; // Default is 3
+const objectList = [
+  { name: 'Boromir', dies: true },
+  { name: 'Elrond', dies: false },
+  { name: 'Frodo', dies: false },
+  { name: 'Gimli', dies: false },
+  { name: 'Legolas', dies: false },
+];
+
+stories.add('Standard - Limit 2', () => (
+  <TruncatedList
+    items={list}
+    limit={limit}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
+  />
+));
+
+stories.add('Standard - Limit Default', () => (
+  <TruncatedList
+    items={list}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
+  />
+));
+
+stories.add('Standard - Only 1 Item', () => (
+  <TruncatedList
+    items={singleItemList}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
+  />
+));
+
+stories.add('Custom Render Props', () => (
+  <TruncatedList
+    items={objectList}
+    link="http://lotr.wikia.com/wiki/Fellowship_of_the_Ring"
+    renderItems={items => (
+      <>
+        {items.map(({ name, dies }) => (
+          <span key={name}>
+            {name} { dies && '(Dead)' },{' '}
+          </span>
+        ))}
+      </>
+    )}
+    renderMore={(numberOfItems: string, link: string) => (
+      <>
+        <a href={link}>and {numberOfItems} more…</a>
+      </>
+    )}
+  />
+));
