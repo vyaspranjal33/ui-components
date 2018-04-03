af2fa2ef9b64242a12e9850a110e6c690dfce821
Marc Missey <marcmissey@gmail.com>
2018-04-03 17:20:05 -0600

    Completed EmailCard components and stories

---
+++ src/components/EmailCard.tsx
@@ -0,0 +1,199 @@
+import React from 'react';
+import cn from '../utilities/classnames';
+import findActiveIndex from '../utilities/find-active-index';
+import Alert from './Alert';
+import {
+  Button,
+  Buttonized,
+} from './Button';
+import Icon from './Icon';
+
+interface EmailCardStatistic {
+  label: string;
+  amount: number;
+  format: string;
+}
+
+const EmailCardStatistics: React.SFC<{ statisticData?: EmailCardStatistic[] }> = ({ statisticData }) => {
+  // This uses numbers rather than formatted strings to prepare for locale specific number formatting
+  const formatStatistic = (amount: number, format: string) => {
+    if (format === 'number') {
+      const parts = amount.toString().split('.');
+      parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+      return parts.join('.');
+    } else {
+      return (amount * 100).toFixed(2) + '%';
+    }
+  };
+
+  const statisticsClassMap = ['', 'delivered', 'unique-opens', 'unique-clicks', 'unsubscribes'];
+  const statisticsElements = statisticData && statisticData.map((stat, i) => {
+    const value = formatStatistic(stat.amount, stat.format);
+    const specificClass = statisticsClassMap[i] || '';
+    return (
+      <div className="email-stat" key={stat.label}>
+        <p className={'stat ' + specificClass}>{value}</p>
+        <p className="label">{stat.label}</p>
+      </div>
+    );
+  });
+
+  return statisticsElements &&
+    (
+      <div className="email-card-stats">
+        {statisticsElements}
+      </div>
+    )
+  || null;
+
+};
+
+interface EmailCardSendTimeProps {
+  value?: string;
+  renderSendTimeLink?: (value: string) => any;
+  alert?: any;
+}
+
+const EmailCardSendTime: React.SFC<EmailCardSendTimeProps> = ({ value, renderSendTimeLink, alert = '' }) => {
+  return (
+    <div className={cn('email-card-send-time', { 'has-value': !!value })}>
+      <Buttonized type="secondary">
+          {renderSendTimeLink && renderSendTimeLink(value)}
+      </Buttonized>
+      {alert}
+    </div>);
+};
+
+interface EmailCardDetail {
+  label: string;
+  value: string;
+  renderEditDetailLink?: (value: string) => any;
+}
+
+const EmailCardDetails: React.SFC<{ details?: EmailCardDetail[] }> = ({ details }) => {
+  const rows = details && details.map((detail) => {
+    return (
+      <tr key={detail.label}>
+        <td className="label">{detail.label}</td>
+        <td>{detail.renderEditDetailLink && detail.renderEditDetailLink(detail.value) || detail.value}</td>
+      </tr>
+    );
+  }) || [];
+
+  return (
+    <div className="email-card-details">
+      <table>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+interface EmailCardContentProps {
+  thumbnailUrl: string;
+  onContentEditClick: (event: any) => void;
+}
+
+const EmailCardContent: React.SFC<EmailCardContentProps> = ({ thumbnailUrl, onContentEditClick }) => (
+  <div className="email-card-content">
+    {
+      thumbnailUrl ?
+        <a href="#">
+          <img src={thumbnailUrl} onClick={onContentEditClick} />
+          <span className="email-card-content-edit">
+            <Icon type="pencil" />
+            Edit
+          </span>
+        </a>
+      :
+        <Button type="secondary" onClick={onContentEditClick}>
+          Add Email Content
+        </Button>
+    }
+  </div>
+);
+
+interface EmailCardAddButtonProps {
+  onClick: (event: any) => void;
+}
+
+export const EmailCardAddButton: React.SFC<EmailCardAddButtonProps> = ({ onClick }) => {
+  return (
+    <div className="btn-list email-card-add">
+      <Button type="secondary" onClick={onClick}>
+        Add an Email
+      </Button>
+    </div>
+  );
+};
+
+interface EmailCardProps {
+  details?: EmailCardDetail[];
+  editing?: boolean;
+  hasAlert?: boolean;
+  isEditable?: boolean;
+  live?: boolean;
+  n: number;
+  onContentEditClick?: (event: any) => void;
+  paused?: boolean;
+  renderSendTimeLink?: (value: string) => any;
+  sendTimeValue?: string;
+  statistics?: EmailCardStatistic[];
+  thumbnailUrl?: string;
+}
+
+export class EmailCard extends React.Component<EmailCardProps> {
+  public static defaultProps = {
+    editing: false,
+    hasAlert: false,
+    isEditable: false,
+    live: false,
+    paused: false,
+  };
+
+  public render() {
+    const saveAlert = this.props.hasAlert && this.renderAlert();
+    return (
+      <div
+        className={cn('email-card-wrap', {
+          'has-alert': this.props.hasAlert,
+          'is-editable': this.props.isEditable,
+          'is-live': this.props.live,
+          'is-paused': this.props.paused,
+        })}
+      >
+        <EmailCardSendTime
+          value={this.props.sendTimeValue}
+          renderSendTimeLink={this.props.renderSendTimeLink}
+          alert={saveAlert}
+        />
+        <EmailCardStatistics statisticData={this.props.statistics}/>
+        <div className="email-card">
+          <div className="email-card-count">
+            <p>Email {this.props.n}</p>
+          </div>
+          <EmailCardContent thumbnailUrl={this.props.thumbnailUrl} onContentEditClick={this.props.onContentEditClick} />
+          <EmailCardDetails details={this.props.details}/>
+        </div>
+        {saveAlert}
+      </div>
+    );
+  }
+  // TODO: Add `dismissable` to remove X button after Chris's PR is merged
+  private renderAlert() {
+    return (
+      <Alert type="warning" icon="info-circle">
+        <p>
+          Changes you've made to this email have not been applied to your live automation.
+          <Button type="primary" small>
+            Save and Apply
+          </Button>
+        </p>
+      </Alert>
+    );
+  }
+}
+
+export default EmailCard;
---
+++ stories/EmailCard.stories.tsx
@@ -0,0 +1,98 @@
+import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import {
+  EmailCard,
+  EmailCardAddButton
+} from '../src/components/EmailCard';
+
+const loaderStories = storiesOf('EmailCard', module);
+
+const statistics = [
+  { label: "Emails Sent", amount: 12230, format: "number" },
+  { label: "Delivered", amount: .966, format: "percent" },
+  { label: "Unique Opens", amount: .802, format: "percent" },
+  { label: "Unique Clicks", amount: .6024, format: "percent" },
+  { label: "Unsubscribes", amount: .0204, format: "percent" }
+];
+
+const renderEditDetailLink = (value: string) => {
+  return (
+    <a href="#">
+      { value }
+    </a>)
+};
+
+const details = [
+  { label: "Subject", value: "Welcome to SendGrid", renderEditDetailLink },
+  { label: "Preheader", value: "Get starting sending up to 12,000 emails Today", renderEditDetailLink },
+  { label: "From Sender", value: "Tyler Hale <tyler.hale@sendgrid.com>", renderEditDetailLink }
+];
+
+const renderSendTimeLink = (value: string) => {
+  return (
+    <a href="#">
+      { value || 'Select Send Time' }
+    </a>)
+};
+
+const onContentEditClick = (e: MouseEvent) => {
+  return true
+};
+
+const cardData = {
+  details,
+  statistics,
+  renderSendTimeLink,
+  onContentEditClick,
+  n: 1
+};
+
+const cardDataNoStats = {
+  details,
+  renderSendTimeLink,
+  onContentEditClick,
+  n: 1
+}
+
+const cardDataWithImage = {
+  details,
+  statistics,
+  thumbnailUrl: "http://via.placeholder.com/128x88",
+  renderSendTimeLink,
+  onContentEditClick,
+  n: 1
+}
+
+loaderStories.add('Email Card', () => ( <EmailCard sendTimeValue="Send Instantly" { ...cardData } /> ));
+
+loaderStories.add("Email Card with No Statistics", () => ( <EmailCard sendTimeValue="Send Instantly" { ...cardDataNoStats } /> ));
+
+loaderStories.add("Email Card with Content", () => ( <EmailCard sendTimeValue="Send Instantly" { ...cardDataWithImage } /> ));
+
+loaderStories.add("Email Card - Edit Mode", () => ( <EmailCard { ...cardDataNoStats } editing /> ));
+
+loaderStories.add("Email Card - Live", () => ( <EmailCard { ...cardData } sendTimeValue="Send Instantly" live /> ));
+
+loaderStories.add("Email Card - Paused", () => ( <EmailCard { ...cardData } sendTimeValue="Send Instantly" paused/> ));
+
+loaderStories.add("Email Card - Multi Editable", () => (
+  <div>
+    <EmailCard { ...cardData } isEditable />
+    <EmailCard { ...cardData } n={2} isEditable />
+    <EmailCard { ...cardData } n={3} isEditable />
+    <EmailCard { ...cardData } n={4} isEditable />
+    <EmailCardAddButton onClick={null} />
+  </div>
+));
+
+loaderStories.add("Email Card - Multi Live", () => (
+  <div>
+    <EmailCard { ...cardData } live sendTimeValue="Send Instantly"/>
+    <EmailCard { ...cardData } n={2} live sendTimeValue="Send 3 Days After Email 1"/>
+    <EmailCard { ...cardData } n={3} live sendTimeValue="Send 3 Days After Email 2"/>
+    <EmailCard { ...cardData } n={4} live sendTimeValue="Send 3 Days After Email 3"/>
+  </div>
+));
+
+loaderStories.add("Email Card with Alerts ", () => ( <EmailCard { ...cardData } live hasAlert sendTimeValue="Send Instantly"/> ));
