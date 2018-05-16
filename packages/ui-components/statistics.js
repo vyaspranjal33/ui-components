5af8d1e700976664e47cc7a26082cc6c01edc24d
Marc Missey <marcmissey@gmail.com>
2018-05-16 11:44:45 -0600

    chore: build packages

--- packages/ui-components/statistics.js
+++ packages/ui-components/statistics.js
@@ -17,30 +17,18 @@ var __rest = (this && this.__rest) || function (s, e) {
 };
 import React from 'react';
 import cn from './utilities/classnames';
+export var NO_STATS_CHAR = '—';
+export var EmailCardStat = function (_a) {
+    var statistic = _a.statistic, specificClass = _a.specificClass, commonClass = _a.commonClass;
+    return (React.createElement("div", { className: commonClass, key: statistic.label },
+        React.createElement("p", { className: cn('stat', specificClass) }, (statistic.amount || parseInt(statistic.amount, 10) === 0) ? statistic.amount : NO_STATS_CHAR),
+        React.createElement("p", { className: "label" }, statistic.label)));
+};
 export var Statistics = function (_a) {
-    var statistics = _a.statistics, statsClassName = _a.statsClassName, className = _a.className, attributes = __rest(_a, ["statistics", "statsClassName", "className"]);
-    // This uses numbers rather than formatted strings to prepare for locale specific number formatting
-    var formatStatistic = function (amount, format) {
-        if (typeof amount !== 'number') {
-            return '—';
-        }
-        if (format === 'number') {
-            return amount.toLocaleString();
-        }
-        else {
-            return (amount * 100).toFixed(2) + '%';
-        }
-    };
-    var statisticsClassMap = ['', 'delivered', 'unique-opens', 'unique-clicks', 'unsubscribes'];
-    var statisticsElements = statistics && statistics.map(function (stat, i) {
-        var value = formatStatistic(stat.amount, stat.format);
-        var specificClass = statisticsClassMap[i] || '';
-        return (React.createElement("div", { className: statsClassName, key: stat.label },
-            React.createElement("p", { className: 'stat ' + specificClass }, value),
-            React.createElement("p", { className: "label" }, stat.label)));
-    });
-    return statisticsElements ?
-        (React.createElement("div", __assign({ className: cn('email-card-stats', className) }, attributes), statisticsElements)) : null;
+    var commonClass = _a.commonClass, className = _a.className, children = _a.children, attributes = __rest(_a, ["commonClass", "className", "children"]);
+    return (React.createElement("div", __assign({ className: cn('email-card-stats', className) }, attributes), React.Children.map(children, function (child) {
+        return React.cloneElement(child, { commonClass: commonClass });
+    })));
 };
 export default Statistics;
 //# sourceMappingURL=statistics.js.map
