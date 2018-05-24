f5b569bcb994886f37768172629b45a3226d664d
Alex Thomsen <alex.scott.thomsen@gmail.com>
2018-05-24 13:38:58 -0600

    Bash magic

--- .buildkite/theDecider.sh
+++ .buildkite/theDecider.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
+# Get where the script is currently running from, through bash magic
+DIRNAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if git log -1 --pretty=%B | cat | grep -e '\[Prepublish Built\]'; then
-    buildkite-agent pipeline upload ./buildPipeline.yml
+    echo "Publish Time" 
+    buildkite-agent pipeline upload $DIRNAME/publishPipeline.yml
 else
-    buildkite-agent pipeline upload ./publishPipeline.yml
+    echo "Need to build" 
+    buildkite-agent pipeline upload $DIRNAME/buildPipeline.yml
 fi
