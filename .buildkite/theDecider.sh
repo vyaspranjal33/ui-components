76bd24e90a8b8a778f516bf9e5874003cd30047d
Alex Thomsen <alex.scott.thomsen@gmail.com>
2018-05-26 10:26:54 -0700

    Try and echo current UID

--- .buildkite/theDecider.sh
+++ .buildkite/theDecider.sh
@@ -3,6 +3,8 @@ set -e
 # Get where the script is currently running from, through bash magic
 DIRNAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+echo$(UID)
+
 if git log -1 --pretty=%B | cat | grep -e '\[Prepublish Built\]'; then
     echo "Publish Time" 
     buildkite-agent pipeline upload $DIRNAME/publishPipeline.yml
