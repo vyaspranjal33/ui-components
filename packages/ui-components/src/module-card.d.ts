1455059bafd8674d87e88604df2a28cd19c1665b
Alex Thomsen <alex.scott.thomsen@gmail.com>
2018-06-01 14:16:30 -0700

    Cleared out the packages folder and removed the old segment-term comp… #minor (#168)
    * Cleared out the packages folder and removed the old segment-term component
    
    * re-add package.json

--- packages/ui-components/src/module-card.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-import React from 'react';
-export interface ModuleCardProps {
-    type: ModuleCardType;
-    className?: string;
-}
-export declare const ModuleCard: React.SFC<ModuleCardProps>;
-export default ModuleCard;
