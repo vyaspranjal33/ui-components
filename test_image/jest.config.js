33d33f06a3d53e35ab99f304f0f3ce32fb36439f
Hannah Apuan <hannah.apuan@gmail.com>
2018-06-29 13:42:34 -0600

    reverted test_image/* files to master

--- test_image/jest.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const globalJestConfig = require('../jest.config');
-
-const finalJestConfig = globalJestConfig;
-
-finalJestConfig.rootDir = path.join(__dirname, '../');
-finalJestConfig.testMatch = ['/**/*ImageStoryshots.ts'];
-
-module.exports = finalJestConfig;
