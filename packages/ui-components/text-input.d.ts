0222e4e7209da28ca51be1204c8bc0e6c6b6b385
sendgrid-ci <jenkins-ci@sengrid.com>
2018-07-02 22:18:16 +0000

    [Prepublish Built]

--- packages/ui-components/text-input.d.ts
+++ packages/ui-components/text-input.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="react" />
 import React, { CSSProperties } from 'react';
 import { InputType } from './types/inputs';
-import { Omit } from './types/utils';
 export interface TextInputProps {
     children?: React.ReactNode;
     type: InputType;
@@ -17,18 +16,10 @@ export interface TextInputProps {
     isSearch?: boolean;
     label?: string;
     info?: string;
-    onBlur?: (event: FocusEvent, value: string | number) => void;
+    onBlur?: (event: any, value: string | number) => void;
     style?: CSSProperties;
 }
-/**
- * Inorder to allow for ...attributes we need to use
- * Partial<React.InputHTMLAttributes<HTMLInputElement>>
- * to add all of those possible types. However, this component is
- * redefining the function definition for onBlur, so we must omit the
- * original event hook from the possible props.
- */
-export declare type HTMLInputElementProps = Partial<Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onBlur'>>;
-export declare class TextInput extends React.Component<TextInputProps & HTMLInputElementProps, {
+export declare class TextInput extends React.Component<TextInputProps, {
     isInputFocused: boolean;
 }> {
     static defaultProps: {
@@ -44,7 +35,7 @@ export declare class TextInput extends React.Component<TextInputProps & HTMLInpu
         value: string;
     };
     onInputFocus: (event: any) => void;
-    constructor(props: TextInputProps & Partial<React.InputHTMLAttributes<HTMLInputElement>>);
+    constructor(props: TextInputProps);
     readonly inputStyle: React.CSSProperties;
     onValueChange(event: any): void;
     onInputBlur(event: any): void;
