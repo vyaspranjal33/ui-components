8b89845a7aebfaa93cefeec2c54ca8713f2edc0b
Steve Kinney <hello@stevekinney.net>
2018-07-23 11:29:56 -0600

    Add onRemove prop to CSV upload (#213)
    ## Merge Checklist
    
    - [x] Code reviewed

--- src/csv-upload.tsx
+++ src/csv-upload.tsx
@@ -1,38 +1,60 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 
 import Dropzone from './dropzone';
 import FileUpload, { DroppedFile, FileSelect } from './file-upload';
 
 export interface CSVUploadProps {
   onChange: (event: any) => void;
+  onRemove?: (event: any) => void;
 }
 
-const CSVUpload: React.SFC<CSVUploadProps> = ({ onChange }) => (
-  <FileUpload
-    onFileSelect={onChange}
-    render={({
-      hasFile,
-      hovered,
-      invalid,
-      file,
-      handleRemove,
-      FileSelectLink,
-    }) => (
-      <Dropzone active={hasFile} hovered={hovered} invalid={invalid}>
-        {file ? (
-          <DroppedFile file={file} onRemove={handleRemove} />
-        ) : (
-          <FileSelect>
-            <Fragment>
-              Drag and drop your CSV file here or{' '}
-              <FileSelectLink>select a CSV file to upload</FileSelectLink>.
-            </Fragment>
-          </FileSelect>
+class CSVUpload extends PureComponent<CSVUploadProps> {
+  public handleRemove = (
+    callback: (event: any) => void
+  ): ((event: any) => void) => {
+    return (event: any): void => {
+      const { onRemove } = this.props;
+      if (onRemove) {
+        onRemove(event);
+      }
+      callback(event);
+    };
+  };
+
+  public render() {
+    const { onChange, onRemove } = this.props;
+
+    return (
+      <FileUpload
+        onFileSelect={onChange}
+        render={({
+          hasFile,
+          handleRemove,
+          hovered,
+          invalid,
+          file,
+          FileSelectLink,
+        }) => (
+          <Dropzone active={hasFile} hovered={hovered} invalid={invalid}>
+            {file ? (
+              <DroppedFile
+                file={file}
+                onRemove={this.handleRemove(handleRemove)}
+              />
+            ) : (
+              <FileSelect>
+                <Fragment>
+                  Drag and drop your CSV file here or{' '}
+                  <FileSelectLink>select a CSV file to upload</FileSelectLink>.
+                </Fragment>
+              </FileSelect>
+            )}
+          </Dropzone>
         )}
-      </Dropzone>
-    )}
-    supportedType={'text/csv'}
-  />
-);
+        supportedType={'text/csv'}
+      />
+    );
+  }
+}
 
 export default CSVUpload;
