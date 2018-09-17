e966adb5c12424e1f561991c9f300562d60dbb1c
sendgrid-ci <jenkins-ci@sengrid.com>
2018-09-17 23:29:15 +0000

    [Prepublish Built]

--- packages/ui-components/table/table-row.js
+++ packages/ui-components/table/table-row.js
@@ -1,6 +1,16 @@
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 import React from 'react';
-export const TableRow = ({ children, className, }) => {
-    return (React.createElement("tr", { className: className }, children));
+export const TableRow = (_a) => {
+    var { children, className } = _a, attributes = __rest(_a, ["children", "className"]);
+    return (React.createElement("tr", Object.assign({ className: className }, attributes), children));
 };
 export default TableRow;
 //# sourceMappingURL=table-row.js.map
