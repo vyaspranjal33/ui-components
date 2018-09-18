80e167cb14919209aada2540d95e0d63ec2c3770
Steve Kinney <hello@stevekinney.net>
2018-09-18 17:22:32 -0600

    Update copyStyles.sh

--- copyStyles.sh
+++ copyStyles.sh
@@ -6,5 +6,8 @@ mkdir -p packages/
 cd src
 
 rsync --relative -r --include="*/" --include="*.scss" --exclude="*" ./ ../packages/ui-components/
+rsync --relative -r --include="*/" --include="*.ttf" --exclude="*" ./ ../packages/ui-components/
+rsync --relative -r --include="*/" --include="*.woff" --exclude="*" ./ ../packages/ui-components/
+rsync --relative -r --include="*/" --include="*.woff2" --exclude="*" ./ ../packages/ui-components/
 rsync -r --prune-empty-dirs  --include="*.d.ts" ./@types/ ../packages/ui-components/@types/
 rsync --relative -r --prune-empty-dirs  --include="*/" --include="*.scss.d.ts" --exclude="*" ./ ../packages/ui-components/@types/
