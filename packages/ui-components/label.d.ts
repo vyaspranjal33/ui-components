c7a2ea00163875476038332c29d56c9c6c137e7f
sendgrid-ci <jenkins-ci@sengrid.com>
2018-09-28 19:51:59 +0000

    [Prepublish Built]

--- packages/ui-components/label.d.ts
+++ packages/ui-components/label.d.ts
@@ -3,7 +3,7 @@ import React from 'react';
 export interface LabelProps {
     className?: string;
     text: string;
-    type: 'sent' | 'delivered' | 'live' | 'success' | 'scheduled' | 'pick-winner' | 'draft' | 'global' | 'list' | 'progress' | 'bounced' | 'canceled' | 'paused' | 'error' | 'optimized';
+    type: 'sent' | 'delivered' | 'live' | 'success' | 'scheduled' | 'pick-winner' | 'draft' | 'global' | 'list' | 'progress' | 'bounced' | 'canceled' | 'disabled' | 'paused' | 'error' | 'optimized';
 }
 export declare const Label: React.SFC<LabelProps>;
 export default Label;
